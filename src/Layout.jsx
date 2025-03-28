d0d2f49e33feba78a9b29d8b6b7a1600add51147
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-03-28 08:29:26 +0100

    just work

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "./Components/AppSideBar";
+import { AppSidebar } from "@/Components/AppSideBar";
 export default function Layout() {
   return (
     <SidebarProvider>
